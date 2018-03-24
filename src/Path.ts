6c30f423ee0cb7bc0c5d8a955d6dc902dcb12638
oliverherrmann <o.herrmann92@gmail.com>
2018-03-24 09:47:18 +0100

    add path and pattern type guards

--- src/Path.ts
+++ src/Path.ts
@@ -1,3 +1,5 @@
+import { isPattern } from "./Pattern"
+
 /**
  * A segment describing an key or index in an object or array
  */
@@ -8,4 +10,33 @@ type PathSegment = string | number
  */
 type Path = PathSegment | PathSegment[]
 
-export { PathSegment, Path }
+/**
+ * Checks if an object is a valid path
+ * @param p the object to check
+ * @returns true if {@code p} is a valid path
+ */
+function isPath(p: any): p is Path {
+  return (
+    isPattern(p) &&
+    new Array<PathSegment>().concat(p as any).every(s => {
+      return (
+        (typeof s === "string" || typeof s === "number") &&
+        s !== "*" &&
+        s !== "**"
+      )
+    })
+  )
+}
+
+/**
+ * Checks if an path contains number-like strings
+ * @param p the path to check
+ * @returns true, if {@code p} contains a number-like string
+ */
+function hasNumberlikeStrings(p: Path) {
+  return new Array<PathSegment>().some(s => {
+    return typeof s === "string" && !isNaN(Number(s))
+  })
+}
+
+export { PathSegment, Path, isPath, hasNumberlikeStrings }
--- src/get.ts
+++ src/get.ts
@@ -1,4 +1,4 @@
-import { Path } from "./Path"
+import { Path, isPath } from "./Path"
 
 /**
  * Retrieves a value in an object described by an path
@@ -13,6 +13,12 @@ import { Path } from "./Path"
  * ```
  */
 function get<T>(path: Path, obj: T): any {
+  if (process && process.env && process.env.NODE_ENV === "development") {
+    if (!isPath(path)) {
+      throw new TypeError(`${path} is not a valid path`)
+    }
+  }
+
   const pathArr: Path = new Array<any>().concat(path)
   return pathArr.reduce((o: any, segment) => {
     if (null !== o && undefined !== o) {
